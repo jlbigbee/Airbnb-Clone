4270c0488d1b381205c0ef62a52f1bbb5ce72878
bigbee <bigbee.jonathan@gmail.com>
2022-09-27 16:13:06 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-        /*●▬▬▬▬๑۩ PACKAGES SETUP ۩๑▬▬▬▬▬●*/
+/*●▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩ PACKAGES SETUP ۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●*/
 
 const express = require('express');
 require('express-async-errors');
@@ -17,8 +17,10 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+const { ValidationError } = require('sequelize');
 
-        /*●▬▬▬▬๑۩ MIDDLEWARE ۩๑▬▬▬▬▬●*/
+
+/*●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩ MIDDLEWARE ۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●*/
 
 // Security Middleware
 if (!isProduction) {
@@ -45,10 +47,44 @@ app.use(
 );
 
 
-        /*●▬▬▬▬๑۩ ROUTES ۩๑▬▬▬▬▬●*/
+/*●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩ ROUTES ۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●*/
 
 const routes = require('./routes');
 app.use(routes);
+
+
+/*●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩ ERROR HANDLERS ۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●*/
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  // Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 
 
