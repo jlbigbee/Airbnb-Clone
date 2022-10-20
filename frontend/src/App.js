dc784bffe2a326516f25057808a353cea892e02d
bigbee <bigbee.jonathan@gmail.com>
2022-10-20 02:19:46 -0700

    phase 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,12 +12,21 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
-    <Switch>
+  return (
+    <>
+    <Navigation isLoaded={isLoaded} />
+
+   {isLoaded && (
+     <Switch>
       <Route path="/login">
         <LoginFormPage />
       </Route>
+      <Route path="/signup">
+            <SignupFormPage />
+      </Route>
     </Switch>
+    )}
+    </>
   );
 }
 
